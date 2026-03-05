13d9a5b6d082cbdbfd1c600a7ce6364480ffc93e
Mari <mjaoshvili@adobe.com>
2026-03-05 14:27:33 -0600

    footer ue

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -55,7 +55,10 @@ function updateCopyrightYear(footerRoot) {
   });
 }
 
-/** Loads footer fragment from footer page; first 5 sections get footer-brand, footer-nav, footer-tagline, footer-social, footer-utility. */
+/**
+ * Loads footer fragment from footer page; first 5 sections get
+ * footer-brand, footer-nav, footer-tagline, footer-social, footer-utility.
+ */
 export default async function decorate(block) {
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
