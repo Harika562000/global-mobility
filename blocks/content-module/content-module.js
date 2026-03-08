54deb38d3f974dbb9ebb3bf7a8328f59a8451262
Shaik Younus <younus123shaik@gmail.com>
2026-03-08 15:55:35 +0530

    s

--- blocks/content-module/content-module.js
+++ blocks/content-module/content-module.js
@@ -102,7 +102,10 @@ export default function decorate(block) {
     moveInstrumentation(col2Source, descriptionDiv);
 
     // Animation logic (only for specific variations)
-    const animTarget = descriptionDiv.querySelector('h4') || descriptionDiv.querySelector('p');
+    const animTarget = descriptionDiv.querySelector('h4 em') || descriptionDiv.querySelector('h4') || descriptionDiv.querySelector('p');
+    if (animTarget && (animTarget.tagName === 'EM' || animTarget.tagName === 'H4')) {
+      animTarget.classList.add('display-text');
+    }
     if (animTarget && (isDefault || isFullWidthHeadline)) {
       wrapWords(animTarget);
       observeScrollReveal(block, () => animateWords(animTarget));
